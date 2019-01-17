d10b11b8ec378719799560411d5753ef74e312cd
dev-carlos-arenas <dev.carlos.arenas@outlook.com>
2019-01-16 18:17:09 -0600

    turtle

---
+++ turtle_automated.py
@@ -0,0 +1,11 @@
+import turtle
+
+cuadarado = 4
+
+tortuga = turtle.Turtle()
+
+for i in range(8):
+    tortuga.forward(50)
+    tortuga.right(90)
+
+turtle.done()
---
+++ turtle_t.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+tortuga = turtle.Turtle()
+
+tortuga.forward(50)
+tortuga.right(90)
+tortuga.forward(150)
+tortuga.left(90)
+tortuga.forward(50)
+tortuga.left(90)
+tortuga.forward(150)
+tortuga.right(90)
+tortuga.forward(50)
+tortuga.left(90)
+tortuga.forward(50)
+tortuga.left(90)
+tortuga.forward(150)
+tortuga.left(90)
+tortuga.forward(50)
+
+turtle.done()
